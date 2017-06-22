25bc7ffa3405cb8afe95580d3f3c1ce85cca94a8
Johnny <Johnny@saijiongtekiair>
2017-06-22 18:16:34 +0800

    test2

--- Mailer.php
+++ Mailer.php
@@ -10,52 +10,44 @@
  * @author Johnny
  */
 class Mailer {
-	/**
-	 * 
-	 *@var boolean 是否使用SMTP
-	 * 
-	 */
+	/** @var boolean 是否使用SMTP */
 	public $isSmtp = false;
 	
-	/**
-	 *
-	 * @var string SMTP伺服器位置
-	 * 
-	 */
+	/** @var string SMTP伺服器位置 */
 	public $smtpServer = 'localhost';
 	
-	/**
-	 *
-	 * @var integer SMTP Port
-	 * 
-	 */
+	/** @var integer SMTP Port */
 	public $port = 25;
 	
 	
-	/**
-	 *
-	 * @var 是否使用帳號登入SMTP
-	 */
+	/** @var boolean 是否使用帳號登入SMTP */
 	public $auth = false;
 	
-	/**
-	 *
-	 * @var SMTP5帳號
-	 */
+	/** @var string SMTP5帳號 */
 	public $username;
 
-	/** 
-	 * 
-	 * @var SMTP5密碼
-	 */
+	/** @var string SMTP5密碼 */
 	public $password;
 	
-	/** 
-	 * 
-	 * @var SMTP連線時間
-	 */
+	/** @var integer SMTP連線時間 */
 	public $timeout = 20;
 	
+	/** @var string 寄件人 */
+	protected $from = '';
 	
 	
+	/** @var string 回覆信箱 */
+	protected $replyTo = '';
+	
+	
+	/**
+	 * @var array 收件人 
+	 */
+	protected $to = array();
+	
+	/** @var array 副本 */
+	protected $cc = array();
+	
+	/** @var array 密件副本 */
+	protected $bcc = array();
 }
